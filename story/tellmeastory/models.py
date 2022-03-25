8965756f4b2999a9c23e3d282f163ae86494b668
Drew Barlow <Drew Barlow>
2022-03-25 16:41:00 -0400

    added display name tests

--- story/tellmeastory/models.py
+++ story/tellmeastory/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username: str = models.CharField(max_length=200)
+    password: str = models.CharField(max_length=512)
+    display_name: str = models.CharField(max_length=200)
