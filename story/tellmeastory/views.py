a944559b8099ebeb846ac8ecac92479549a2adcd
Drew Barlow <Drew Barlow>
2022-03-25 15:52:32 -0400

    Added basic temporary index

--- story/tellmeastory/views.py
+++ story/tellmeastory/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+# temp, obviously
+def index(req) -> HttpResponse:
+    return HttpResponse("Tell Me a Story: TEMP INDEX")
