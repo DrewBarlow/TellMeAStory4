57bd771b385028b4c595acd5083a8fe68ba3adc1
Drew Barlow <Drew Barlow>
2022-03-27 17:31:43 -0400

    Updated Account view and HTML page

--- story/tellmeastory/forms.py
+++ story/tellmeastory/forms.py
@@ -11,3 +11,7 @@ class RegisterForm(Form):
 
     # default to the username if not specified
     display_name: CharField = CharField(max_length=200, required=False)
+
+class NameChangeForm(Form):
+    # this may not need to be required?
+    new_display_name: CharField = CharField(max_length=200, required=True)
