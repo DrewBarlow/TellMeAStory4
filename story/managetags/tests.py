8df98fa431d760f8e450023dadfb2bc3714bcce3
Shah-Kush <k216@umbc.edu>
2022-03-29 22:41:35 -0400

    Added skeletons for Tag tests.

--- story/managetags/tests.py
+++ story/managetags/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
-
+# TODO: from .models import Tag
 # Create your tests here.
+class TagModelTests(TestCase):
+    def test_was_tag_name_correct(self):
+        # Returns False for tags created with incorrect
+        # names when given a name.
+        is_name_correct = False
+        self.assertIs(is_name_correct, True)
+
+    def test_was_tag_ID_correct(self):
+        # Returns False for tags created with incorrect
+        # ID when given an ID.
+        is_ID_correct = False
+        self.assertIs(is_ID_correct, True)
+
+    def test_was_tag_added(self):
+        # Returns False for tags that don't exist in the db
+        # after insertion.
+        tag_in_db = False
+        self.assertIs(tag_in_db, True)
+
+    def test_was_tag_added_without_repeat(self):
+        # Returns False for tags that don't reject
+        # insertion when repeated.
+        tag_in_db = False
+        tag_repeated = False
+        self.assertIs(tag_repeated, False)
