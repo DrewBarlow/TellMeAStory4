2721e5b248f5aa80156cf1c83e82be892470a96d
Shah-Kush <k216@umbc.edu>
2022-03-30 17:26:10 -0400

    Updated launch page to registration page

--- story/tellmeastory/urls.py
+++ story/tellmeastory/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = "tellmeastory"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.register, name="register"),
+    path("index/", views.index, name="index"),
     path("account/<str:username>/", views.account, name="account_page"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
