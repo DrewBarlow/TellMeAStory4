92998aabaa019b028e7b8379e0feee4297cc0905
Shah-Kush <k216@umbc.edu>
2022-03-30 12:12:07 -0400

    Added addtags url to tellmeastory urls

--- story/tellmeastory/urls.py
+++ story/tellmeastory/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = "tellmeastory"
@@ -7,4 +7,5 @@ urlpatterns = [
     path("account/<str:username>/", views.account, name="account_page"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
+    path("addtags/", include("managetags.urls"))
 ]
