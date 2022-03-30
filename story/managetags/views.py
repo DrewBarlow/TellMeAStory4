1a6af9fa04dedb260500024a214eb47b9747996c
Shah-Kush <k216@umbc.edu>
2022-03-30 12:15:14 -0400

    Added url and view for creating tags

--- story/managetags/views.py
+++ story/managetags/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# TODO: Remove temp Index page
+def index(request):
+    return HttpResponse("Temporary Index, visit addtags/create")
+
+# Create tag submission form view
+def create(request):
+    return HttpResponse("Create Tags")
