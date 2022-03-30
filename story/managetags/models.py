5f7a509faf46aeab07e9d5d354d0eba36761d434
Shah-Kush <k216@umbc.edu>
2022-03-30 11:27:35 -0400

    Added exceptions for insertion of Tags

--- story/managetags/models.py
+++ story/managetags/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from enchant import Dict
 
 # Create your models here.
 class Tag(models.Model):
     # TODO: Add many to one relationship with Node model using ForeignKey
     name_text = models.CharField(max_length=255) # tag name identifier
-    ID = models.IntegerField(default=-1) # unique tag ID
+    countID = models.IntegerField(default=-1) # unique tag ID
     usage_count = models.IntegerField(default=0) # times used by users
     language = models.CharField(max_length=255) # language used by tag
 
@@ -16,8 +15,8 @@ class Tag(models.Model):
     # Returns True if tag name is valid alphanumeric and
     # greater than 3 characters (valid tag name).
     def is_valid_name(self) -> bool:
-        MIN_LEN = 3 # minimum Tag name length
-        if self.name_text.isalnum() and len(self.name_text) >= MIN_LEN:
+        MIN_LEN = 3  # minimum Tag name length
+        if str(self.name_text).isalnum() and len(str(self.name_text)) >= MIN_LEN:
             return True
         return False
 
@@ -44,41 +43,34 @@ class Tag(models.Model):
         # tag to the database. Return True if
         # added or False if not added.
         if self.is_valid_name() and self.is_new_tag():
-            self.ID = Tag.objects.count()
+            self.countID = Tag.objects.count()
             self.save()
             return True
         return False
-
-    # Returns True if name is found in a dictionary
-    # given a language.
-    def is_tag_name_in_dictionary(self) -> bool:
-        try:
-            dictionary = Dict(self.language())
-            is_valid_word = dictionary.check(self.name_text)
-            return is_valid_word
-        except:
-            return False
 
     # Returns properties needed to add tag to a node and
     # increments usage count when used.
     def add_tag_to_node(self) -> dict:
         # TODO: Add Tag relation to Node in database
         # Increment usage count and save to db
-        self.usage_count = self.usage_count + 1
-        self.save(update_fields="usage_count")
-
+        try:
+            existing_tag = Tag.objects.get(name_text=self.name_text)
+            existing_tag.usage_count += 1
+            existing_tag.save()
+        except:
+            return {}
         # Return necessary details needed for the
         # connection of a Tag to a node.
-        return {"name_text": self.name_text, "ID": self.ID, "id": self.id}
+        return {"name_text": self.name_text, "countID": self.countID, "id": self.id}
 
     # Decrements usage count when add fails or user removes
     # their tag. Managed by user of Tag model. Returns new
     # usage count.
-    def decrement_usage(self) -> int:
+    def decrement_usage(self):
         # Check if usage_count is already at zero
         if self.usage_count == 0:
             return self.usage_count
         # Decrement usage_count of Tag
-        self.usage_count = self.usage_count - 1
+        self.usage_count -= 1
         # Return updated usage_count
         return self.usage_count
