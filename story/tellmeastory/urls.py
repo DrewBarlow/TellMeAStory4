79ff212be498ca45217615f76d7b2a44dcdfb907
Jason Nachman <jason.nachman@paywithfour.com>
2022-03-30 20:18:32 -0400

    Added map configuration and resolved prior issue

--- story/tellmeastory/urls.py
+++ story/tellmeastory/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("account/<str:username>/", views.account, name="account_page"),
     path("login/", views.login, name="login"),
     path("register/", views.register, name="register"),
+    path("map/" , views.map , name="map") ,
 ]
