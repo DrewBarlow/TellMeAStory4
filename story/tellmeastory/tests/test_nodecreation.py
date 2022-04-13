2c59a276e53497104ca564415e7a7b615e27fd2e
Drew Barlow <Drew Barlow>
2022-04-13 12:29:47 -0400

    wrote node creation view tests

--- story/tellmeastory/tests/test_nodecreation.py
+++ story/tellmeastory/tests/test_nodecreation.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 from django.test import TestCase
 from tellmeastory.models import Node, User
 
+COOKIE_NAME: str = "StoryUserLoggedIn"
+USERNAME: str = "SomeUser"
+
 def insert_registered_user(username: str, password: str, dname: str) -> User:
     return User.objects.create(
         username=username,
@@ -10,4 +13,91 @@ def insert_registered_user(username: str, password: str, dname: str) -> User:
     )
 
 class NodeCreationViewTests(TestCase):
-    pass
+    def setUp(self) -> None:
+        insert_registered_user(USERNAME, "Cringe", "SomeUserDisp")
+        return
+
+    def test_node_creation_page_not_logged_in(self) -> None:
+        """
+        The base creation page should not have a form present.
+        Furthermore, it should have a redirect to the login page.
+        """
+        res: HttpResponse = self.client.get("/story/create-node/")
+        self.assertEqual(res.status_code, 200)
+        self.assertNotContains(res, "Invalid title.")
+        self.assertNotContains(res, "must be less than 10,000")
+        self.assertNotContains(res, "Submit Story")
+        self.assertContains(res, "Log in here.")
+
+        return
+
+    def test_node_creation_page_logged_in(self) -> None:
+        """
+        If the user is logged in, the node creation page should give them
+        a prompt to enter some title and some content for a Node.
+        """
+        self.client.cookies[COOKIE_NAME] = USERNAME
+
+        res: HttpResponse = self.client.get("/story/create-node/")
+        self.assertEqual(res.status_code, 200)
+        self.assertNotContains(res, "Invalid title.")
+        self.assertNotContains(res, "must be less than 10,000")
+        self.assertContains(res, "Submit Story")
+        self.assertNotContains(res, "Log in here.")
+
+        return
+
+    def test_node_creation_bad_title(self) -> None:
+        """
+        An extension of the is_valid_title() Node model test.
+        If the title entered is invalid, then the response should contain "Invalid title."
+        """
+        self.client.cookies[COOKIE_NAME] = USERNAME
+
+        res: HttpResponse = self.client.post("/story/create-node/", data={
+            "node_title": "j",
+            "node_content": "hi"
+        })
+
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, "Invalid title.")
+        self.assertNotContains(res, "must be less than 10,000")
+
+        return
+
+    def test_node_creation_bad_content(self) -> None:
+        """
+        An extension of the is_valid_content() Node model test.
+        If the content entered is invalid, the response should contain the following:
+        "The content must be less than 10,000 characters!"
+        """
+        self.client.cookies[COOKIE_NAME] = USERNAME
+
+        res: HttpResponse = self.client.post("/story/create-node/", data={
+            "node_title": "Hello there bucko",
+            "node_content": "yo" * 10000
+        })
+
+        self.assertEqual(res.status_code, 200)
+        self.assertNotContains(res, "Invalid title.")
+        self.assertContains(res, "must be less than 10,000")
+
+        return
+
+    def test_node_creation_all_valid(self) -> None:
+        """
+        No error message should be present if the user successfully creates a node.
+        TEMPORARILY, THE USER SHOULD BE REDIRECTED TO THE INDEX PAGE. THIS WILL CHANGE.
+        """
+        self.client.cookies[COOKIE_NAME] = USERNAME
+
+        res: HttpResponse = self.client.post("/story/create-node/", data={
+            "node_title": "hows it going",
+            "node_content": "klsjhgkjdshgkjhshk"
+        })
+
+        # redirect
+        self.assertEqual(res.status_code, 302)
+        self.assertEqual(res.get("location"), "/story/")
+
+        return
