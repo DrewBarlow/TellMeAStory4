d8f6314c0fd40add03f0c69e97459e9cf58a0e4d
kwrigh8176 <kwright4@umbc.edu>
2022-04-13 12:42:13 -0400

    Basic Admin Report Page

--- story/tellmeastory/models.py
+++ story/tellmeastory/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=512)
     display_name = models.CharField(max_length=200)
-
+    admin = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
@@ -84,7 +84,7 @@ class Report(models.Model):
     #the reason the user was reported (text field)
     report_reason = models.CharField(max_length=600)
 
-
+    report_id = models.CharField(max_length=100, default="")
 
     def __str__(self):
         return self.report_reason
